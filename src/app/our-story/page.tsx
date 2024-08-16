8cb41649b38655beaeb28b01ecc6e33a19e90371
peytono <pcopcoeco@gmail.com>
2024-08-16 09:02:12 -0500

    add our story text and anchor

---
+++ src/app/our-story/page.tsx
@@ -0,0 +1,31 @@
+export default function ourStory() {
+  return (
+    <div>
+      <h1>Our Story</h1>
+      <p>
+        Originally from Maine, I started making pizza at home with my pops. It was also pops who
+        introduced me to the best spots around New York, New Haven and Boston. Then in '94, we moved
+        to New Orleans. In high school, my first jobs were at a few local pizza joints washing
+        dishes, tossing dough, and making pies. But it was our homemade pies that really hit the
+        spot. There’s just something special about a thin, crispy charred crust that can’t be beat.
+        <br />
+        Later in college, I used what I learned from work and friends (we miss you stikfigg) to make
+        my own dough and started crankin out some pretty killer pies in my apartment. Our weekend
+        pizza parties were really great for about 10 years! Most of what we do today was developed
+        in our small house on Oak Street.
+        <br />
+        Everything changed when the pandemic hit. My camera department film job completely shut
+        down, and we'd just had our baby boy Asher. I was at a crossroads. So I just started making
+        pizza, ALL THE TIME. What began as just giving pies away from the side door, turned into a
+        pop-up at Zony Mash. Even after film production restarted, I had gone too far. There was no
+        turning back, and now Zee's is all grown up into a real pizza shop!
+        <br />
+        We’ve come a long way since that first batch of dough in 2010, I really hope you enjoy it.
+        <br />
+        Thank you, <br />
+        -Zee
+      </p>
+      <a href='https://www.youtube.com/watch?v=FYudAgdhAPo'>Check our OneBite review by Dave Portnoy El Press!</a>
+    </div>
+  );
+}
