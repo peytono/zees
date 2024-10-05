d0a3e56244b01465ebdac6387fec2501ae1afa36
peytono <pcopcoeco@gmail.com>
2024-10-05 12:07:23 -0500

    refactor modal display, add links to flex

--- src/app/Navigation.tsx
+++ src/app/Navigation.tsx
@@ -1,26 +1,32 @@
 'use client';
-import { ReactElement, ReactNode } from 'react';
+import { useState } from 'react';
 import { Menu, X } from 'react-feather';
 import Link from 'next/link';
 
 export default function Navigation() {
-  const handleNavigation = () => {
-    const navigation: HTMLElement | null = document.querySelector('.navigation');
-    if (navigation)
-      navigation?.style.display === 'none'
-        ? (navigation.style.display = 'block')
-        : (navigation.style.display = 'none');
+  const [display, setDisplay] = useState('hidden');
+
+  const handleModalDisplay = () => {
+    setDisplay((prev) => (prev === 'hidden') ? 'block' : 'hidden');
   };
 
   return (
     <>
-      <div className="navigation">
-        <X onClick={handleNavigation} />
-        <Link onClick={handleNavigation} href="/menu">Menu</Link>
-        <Link onClick={handleNavigation} href="/our-pizza">Our Pizza</Link>
-        <Link onClick={handleNavigation} href="/our-story">Our Story</Link>
+      <div className={`navigation overflow-y-hidden ${display}` }>
+        <X onClick={handleModalDisplay} />
+        <div className='my-4 flex flex-col items-center space-y-3'>
+          <Link onClick={handleModalDisplay} href="/menu">
+            Menu
+          </Link>
+          <Link onClick={handleModalDisplay} href="/our-pizza">
+            Our Pizza
+          </Link>
+          <Link onClick={handleModalDisplay} href="/our-story">
+            Our Story
+          </Link>
+        </div>
       </div>
-      <div onClick={handleNavigation}>
+      <div onClick={handleModalDisplay}>
         <Menu size="32" />
       </div>
     </>
